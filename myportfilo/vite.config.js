c66220881a04e08601c8877143d2fb83c86cd94f
sanjaysanjaysa <sanjay33556y@gmail.com>
2025-08-01 18:29:20 +0530

    fix: correct base path for GitHub Pages

--- myportfilo/vite.config.js
+++ myportfilo/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Correct single export
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Portfolio/",// Use your repo name here
+});
